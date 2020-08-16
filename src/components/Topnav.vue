765844ea6595c80aba8a953a2e70c75450e7a4d9
sparks42 <958575100@qq.com>
2020-08-16 23:08:53 +0800

    完善 aside 切换逻辑

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="topnav">
-    <div class="logo"
-         @click="toggleMenu">LOGO</div>
+    <div class="logo">LOGO</div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
     </ul>
-    <span class="toggleAside"></span>
+    <span class="toggleAside"
+          @click="toggleMenu"></span>
   </div>
 </template>
 <script lang="ts">
@@ -43,7 +43,7 @@ export default {
     }
   }
   > .toggleAside {
-    display: inline-block;
+    display: none;
     width: 24px;
     height: 24px;
     background: red;
@@ -58,6 +58,9 @@ export default {
     }
     > .logo {
       margin: 0 auto;
+    }
+    > .toggleAside {
+      display: inline-block;
     }
   }
 }
