c4692fabe7b37486e20e03f2b327405c44af7ba8
sparks42 <958575100@qq.com>
2020-08-29 16:11:38 +0800

    switch css 2

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -26,7 +26,7 @@ button {
   height: $h;
   width: $h * 2;
   border: none;
-  background: grey;
+  background: #bfbfbf;
   border-radius: $h/2;
   position: relative;
 }
@@ -39,11 +39,11 @@ span {
   width: $h2;
   background: white;
   border-radius: $h2 / 2;
-  transition: left 250ms;
+  transition: all 250ms;
 }
 
 button.checked {
-  background: blue;
+  background: #1890ff;
 }
 
 button.checked > span {
@@ -51,5 +51,16 @@ button.checked > span {
 }
 button:focus {
   outline: none;
+}
+button:active {
+  > span {
+    width: $h2 + 4px;
+  }
+}
+button.checked:active {
+  > span {
+    width: $h2 + 4px;
+    margin-left: -4px;
+  }
 }
 </style>
