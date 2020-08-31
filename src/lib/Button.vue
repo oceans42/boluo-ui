1267d0c71ddf7ee7b5c1d122ed23aec00b38dfc7
sparks42 <958575100@qq.com>
2020-08-31 23:17:07 +0800

    CSS最小影响原则

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,6 +1,6 @@
 <template>
   <button class="gulu-button"
-          :class="`theme-${theme}`">
+          :class="`gulu-theme-${theme}`">
     <slot />
   </button>
 </template>
@@ -14,7 +14,7 @@ export default {
   },
 };
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
 $h: 32px;
 $border-color: #d9d9d9;
 $color: #333;
--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,6 +1,7 @@
 <template>
-  <button @click="toggle"
-          :class="{checked: value}"><span></span></button>
+  <button class="gulu-switch"
+          @click="toggle"
+          :class="{'gulu-checked': value}"><span></span></button>
 </template>
 
 <script lang="ts">
@@ -18,11 +19,11 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 $h: 22px;
 $h2: $h - 4px;
 
-button {
+.gulu-switch {
   height: $h;
   width: $h * 2;
   border: none;
@@ -39,7 +40,7 @@ button {
     border-radius: $h2 / 2;
     transition: all 250ms;
   }
-  &.checked {
+  &.gulu-checked {
     background: #1890ff;
     > span {
       left: calc(100% - #{$h2} - 2px);
@@ -54,7 +55,7 @@ button {
       width: $h2 + 4px;
     }
   }
-  &.checked:active {
+  &.gulu-checked:active {
     > span {
       width: $h2 + 4px;
       margin-left: -4px;
