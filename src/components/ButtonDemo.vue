08114e15142048a4037f8ae6711b244f26b25c35
sparks42 <958575100@qq.com>
2020-09-01 23:15:26 +0800

    支持 level 属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -15,13 +15,51 @@
     <Button>普普通</Button>
     <Button size="small">小小小</Button>
   </div>
+  <div>
+    <Button theme="link"
+            size="big">大大大</Button>
+    <Button theme="link">普普通</Button>
+    <Button size="small"
+            theme="link">小小小</Button>
+  </div>
+  <div>
+    <Button size="big"
+            theme="text">大大大</Button>
+    <Button theme="text">普普通</Button>
+    <Button size="small"
+            theme="text">小小小</Button>
+  </div>
+  <h1>示例3</h1>
+  <div>
+    <dir>
+      <Button level="main">主要按钮</Button>
+      <Button>普通按钮</Button>
+      <Button level="danger">危险按钮</Button>
+    </dir>
+    <dir>
+      <Button theme="link"
+              level="main">主要链接按钮</Button>
+      <Button theme="link">普通链接按钮</Button>
+      <Button theme="link"
+              level="danger">危险链接按钮</Button>
+    </dir>
+    <dir>
+      <Button theme="text"
+              level="main">主要文字按钮</Button>
+      <Button theme="text">普通文字按钮</Button>
+      <Button theme="text"
+              level="danger">危险文字按钮</Button>
+    </dir>
+  </div>
 </template>
 <script lang="ts">
 import Button from "../lib/Button.vue";
 export default {
   components: { Button },
   setup() {
-    const onClick = () => {};
+    const onClick = () => {
+      console.log("hi");
+    };
     return { onClick };
   },
 };
