41f19b2cd1e31a26ec99b396bf2234120313200e
sparks42 <958575100@qq.com>
2020-09-09 21:45:09 +0800

    优化2

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -4,7 +4,7 @@
          ref="container">
       <div class="gulu-tabs-nav-item"
            v-for="(t,index) in titles"
-           :ref="el => { if (el) navItems[index] = el }"
+           :ref="el => { if (t===selected) selectedItem = el }"
            @click="select(t)"
            :class="{selected: t=== selected}"
            :key="index">{{t}}</div>
@@ -22,7 +22,7 @@
 
 <script lang="ts">
 import Tab from "./Tab.vue";
-import { computed, ref, onMounted, onUpdated } from "vue";
+import { computed, ref, onMounted, onUpdated, watchEffect } from "vue";
 export default {
   props: {
     selected: {
@@ -30,24 +30,17 @@ export default {
     },
   },
   setup(props, context) {
-    const navItems = ref<HTMLDivElement[]>([]);
+    const selectedItem = ref<HTMLDivElement>(null);
     const indicator = ref<HTMLDivElement>(null);
     const container = ref<HTMLDivElement>(null);
-    const x = () => {
-      const divs = navItems.value;
-      const result = divs.filter((div) =>
-        div.classList.contains("selected")
-      )[0];
-      console.log(result);
-      const { width } = result.getBoundingClientRect();
+    watchEffect(() => {
+      const { width } = selectedItem.value.getBoundingClientRect();
       indicator.value.style.width = width + "px";
       const { left: left1 } = container.value.getBoundingClientRect();
-      const { left: left2 } = result.getBoundingClientRect();
+      const { left: left2 } = selectedItem.value.getBoundingClientRect();
       const left = left2 - left1;
       indicator.value.style.left = left + "px";
-    };
-    onMounted(x);
-    onUpdated(x);
+    });
     const defaults = context.slots.default();
     defaults.forEach((tag) => {
       if (tag.type !== Tab) {
@@ -70,7 +63,7 @@ export default {
       titles,
       current,
       select,
-      navItems,
+      selectedItem,
       indicator,
       container,
     };
