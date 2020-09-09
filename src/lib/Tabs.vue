13ecf167c118b5b95a83b000305693b150e400ea
sparks42 <958575100@qq.com>
2020-09-09 22:13:58 +0800

    采用尤雨溪的建议

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -12,10 +12,8 @@
            ref="indicator"></div>
     </div>
     <div class="gulu-tabs-content">
-      <component class="gulu-tabs-content-item"
-                 :class="{selected: c.props.title === selected }"
-                 v-for="c in defaults"
-                 :is="c" />
+      <component :is="current"
+                 :key="current.props.title" />
     </div>
   </div>
 </template>
@@ -33,6 +31,7 @@ export default {
     const selectedItem = ref<HTMLDivElement>(null);
     const indicator = ref<HTMLDivElement>(null);
     const container = ref<HTMLDivElement>(null);
+
     onMounted(() => {
       watchEffect(() => {
         const { width } = selectedItem.value.getBoundingClientRect();
@@ -49,6 +48,9 @@ export default {
         throw new Error("Tabs 子标签必须是 Tab");
       }
     });
+    const current = computed(() => {
+      return defaults.find((tag) => tag.props.title === props.selected);
+    });
     const titles = defaults.map((tag) => {
       return tag.props.title;
     });
@@ -56,6 +58,7 @@ export default {
       context.emit("update:selected", title);
     };
     return {
+      current,
       defaults,
       titles,
       select,
@@ -100,12 +103,6 @@ $border-color: #d9d9d9;
   }
   &-content {
     padding: 8px 0;
-    &-item {
-      display: none;
-      &.selected {
-        display: block;
-      }
-    }
   }
 }
 </style>
