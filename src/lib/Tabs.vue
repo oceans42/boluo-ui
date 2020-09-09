58c211563064a2f0a09041721687fd46891a1763
sparks42 <958575100@qq.com>
2020-09-09 22:01:05 +0800

    修复 selectedItem.value is null

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -22,7 +22,7 @@
 
 <script lang="ts">
 import Tab from "./Tab.vue";
-import { computed, ref, watchEffect } from "vue";
+import { computed, ref, watchEffect, onMounted } from "vue";
 export default {
   props: {
     selected: {
@@ -33,13 +33,15 @@ export default {
     const selectedItem = ref<HTMLDivElement>(null);
     const indicator = ref<HTMLDivElement>(null);
     const container = ref<HTMLDivElement>(null);
-    watchEffect(() => {
-      const { width } = selectedItem.value.getBoundingClientRect();
-      indicator.value.style.width = width + "px";
-      const { left: left1 } = container.value.getBoundingClientRect();
-      const { left: left2 } = selectedItem.value.getBoundingClientRect();
-      const left = left2 - left1;
-      indicator.value.style.left = left + "px";
+    onMounted(() => {
+      watchEffect(() => {
+        const { width } = selectedItem.value.getBoundingClientRect();
+        indicator.value.style.width = width + "px";
+        const { left: left1 } = container.value.getBoundingClientRect();
+        const { left: left2 } = selectedItem.value.getBoundingClientRect();
+        const left = left2 - left1;
+        indicator.value.style.left = left + "px";
+      });
     });
     const defaults = context.slots.default();
     defaults.forEach((tag) => {
