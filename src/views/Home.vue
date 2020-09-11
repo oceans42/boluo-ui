9c0c0e079a80808c173c26abe44195c3a72e13cc
sparks42 <958575100@qq.com>
2020-09-11 21:46:42 +0800

    features 样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,15 +12,29 @@
       </div>
     </div>
     <div class="features">
-      <svg class="icon">
-        <use xlink:href="#icon-vue"></use>
-      </svg>
-      <svg class="icon">
-        <use xlink:href="#icon-ts"></use>
-      </svg>
-      <svg class="icon">
-        <use xlink:href="#icon-light"></use>
-      </svg>
+      <ul>
+        <li>
+          <svg>
+            <use xlink:href="#icon-vue"></use>
+          </svg>
+          <h3>基于 Vue 3</h3>
+          <p>骄傲地使用了 Vue 3 Composition API</p>
+        </li>
+        <li>
+          <svg>
+            <use xlink:href="#icon-ts"></use>
+          </svg>
+          <h3>基于 TypeScript </h3>
+          <p>源代码采用 TypeScript 书写（非严格检查）</p>
+        </li>
+        <li>
+          <svg>
+            <use xlink:href="#icon-light"></use>
+          </svg>
+          <h3>代码易读</h3>
+          <p>每个组件的源代码都极其简洁</p>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -44,29 +58,39 @@ $color: #007974;
   clip-path: ellipse(80% 60% at 50% 40%);
 }
 .features {
-  > svg {
-    width: 64px;
-    height: 64px;
+  margin: 64px auto;
+  width: 400px;
+  @media (min-width: 800px) {
+    width: 800px;
   }
-}
-.banner {
-  color: $color;
-  padding: 100px 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  > .actions {
-    padding: 8px 0;
-    a {
-      margin: 0 8px;
-      background: $green;
-      color: white;
-      display: inline-block;
-      padding: 8px 24px;
-      border-radius: $border-radius;
-      &:hover {
-        text-decoration: none;
+  @media (min-width: 1200px) {
+    width: 1200px;
+  }
+  > ul {
+    display: flex;
+    flex-wrap: wrap;
+    > li {
+      width: 400px;
+      margin: 16px 0;
+      display: grid;
+      justify-content: start;
+      align-content: space-between;
+      grid-template-areas:
+        "icon title"
+        "icon text";
+      grid-template-columns: 80px auto;
+      grid-template-rows: 1fr auto;
+      > svg {
+        grid-area: icon;
+        width: 64px;
+        height: 64px;
+      }
+      > h3 {
+        grid-area: title;
+        font-size: 28px;
+      }
+      > p {
+        grid-area: text;
       }
     }
   }
