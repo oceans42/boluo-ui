2632ec4d9e1f209e962a5c4a4e3f82445f27aea2
sparks42 <958575100@qq.com>
2020-10-14 22:53:44 +0800

    路由换成 history 模式

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,4 @@
-import { createWebHashHistory, createRouter } from 'vue-router'
+import { createWebHistory, createRouter } from 'vue-router'
 import Home from './views/Home.vue'
 import Doc from './views/Doc.vue'
 import SwitchDemo from './components/SwitchDemo.vue'
@@ -7,7 +7,8 @@ import DialogDemo from './components/DialogDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
 import DocDemo from './components/DocDemo.vue'
 
-const history = createWebHashHistory()
+// const history = createWebHashHistory()
+const history = createWebHistory() // 用 history 模式
 export const router = createRouter({
   history: history,
   routes: [
