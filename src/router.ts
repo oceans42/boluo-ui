114e84e945615288c66b2dd1a5ce2ca7e154117e
sparks42 <958575100@qq.com>
2020-10-18 21:52:07 +0800

    创建路由

--- src/router.ts
+++ src/router.ts
@@ -7,6 +7,10 @@ import DialogDemo from './components/DialogDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
 import DocDemo from './components/DocDemo.vue'
 
+import Intro from './views/Intro.vue';
+import GetStarted from './views/GetStarted.vue'
+import Install from './views/Install.vue'
+
 // const history = createWebHashHistory()
 const history = createWebHistory() // 用 history 模式
 export const router = createRouter({
@@ -18,6 +22,9 @@ export const router = createRouter({
       component: Doc,
       children: [ // 子路由
         { path: '', component: DocDemo },
+        { path: "intro", component: Intro },
+        { path: "get-started", component: GetStarted },
+        { path: "install", component: Install },
         { path: 'switch', component: SwitchDemo },
         { path: 'button', component: ButtonDemo },
         { path: 'dialog', component: DialogDemo },
