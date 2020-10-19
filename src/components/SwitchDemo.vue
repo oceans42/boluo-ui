095757e3cc35997366c0b5bd1281af713e0e4e0f
sparks42 <958575100@qq.com>
2020-10-19 22:59:41 +0800

    更新 SwitchDemo

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,21 +1,86 @@
 <template>
   <div>
-    <h2>示例1</h2>
-    <Switch v-model:value="bool" />
-    <h2>示例2 Heart Switch</h2>
-    <SwitchPro v-model:value="bool2" />
+    <h1>Switch 组件示例 </h1>
+    <div class="demo">
+      <h2>常规用法</h2>
+      <div class="demo-component">
+        <Switch v-model:value="bool" />
+      </div>
+      <div class="demo-actions">
+        <Button>查看代码</Button>
+      </div>
+      <div class="demo-code">
+        <pre>&lt;Switch v-model:value="bool" /&gt;</pre>
+      </div>
+    </div>
+    <div class="demo">
+      <h2>支持 disabled </h2>
+      <div class="demo-component">
+        <Switch v-model:value="bool"
+                disabled />
+      </div>
+      <div class="demo-actions">
+        <Button>查看代码</Button>
+      </div>
+      <div class="demo-code">
+        <pre>&lt;Switch v-model:value="bool" disabled /&gt;</pre>
+      </div>
+    </div>
+    <div class="demo">
+      <h2>心型 Switch</h2>
+      <div class="demo-component">
+        <SwitchPro v-model:value="bool2" />
+      </div>
+      <div class="demo-actions">
+        <Button>查看代码</Button>
+      </div>
+      <div class="demo-code">
+        <pre>&lt;SwitchPro v-model:value="bool2" /&gt;</pre>
+      </div>
+    </div>
   </div>
 </template>
 <script lang="ts">
 import Switch from "../lib/Switch.vue";
 import SwitchPro from "../lib/SwitchPro.vue";
+import Button from "../lib/Button.vue";
 import { ref } from "vue";
+
 export default {
-  components: { Switch, SwitchPro },
+  components: { Switch, SwitchPro, Button },
   setup() {
-    const bool = ref(true);
-    const bool2 = ref(true);
+    const bool = ref(false);
+    const bool2 = ref(false);
     return { bool, bool2 };
   },
 };
 </script>
+
+<style lang="scss" scoped>
+$border-color: #d9d9d9;
+.demo {
+  border: 1px solid $border-color;
+  margin: 16px 0 32px;
+  > h2 {
+    font-size: 20px;
+    padding: 8px 16px;
+    border-bottom: 1px solid $border-color;
+  }
+  &-component {
+    padding: 16px;
+  }
+  &-actions {
+    padding: 8px 16px;
+    border-top: 1px dashed $border-color;
+  }
+  &-code {
+    padding: 8px 16px;
+    border-top: 1px dashed $border-color;
+    > pre {
+      line-height: 1.1;
+      font-family: Consolas, "Courier New", Courier, monospace;
+      margin: 0;
+    }
+  }
+}
+</style>
