5c29255444b38d0416c0cf669592db831f14a3d9
sparks42 <958575100@qq.com>
2020-10-19 18:22:19 +0800

    引入 github 样式

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { createApp } from 'vue' // 创建app实例的函数
 import App from './App.vue'
 import { router } from './router'
 import './lib/svg.js'
+import 'github-markdown-css'
 
 const app = createApp(App)
 app.use(router)
