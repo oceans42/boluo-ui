a913c3ebdc73e93b5f2d11cf04ed2d8927fa58c5
sparks42 <958575100@qq.com>
2020-10-20 19:53:17 +0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@ cd dist &&
 git init &&
 git add . &&
 git commit -m "update" &&
-git branch -M master &&
-git remote add origin git@github.com:FrankFang/gulu-ui-website-1.git &&
-git push -f -u origin master &&
+git branch -M main &&
+git remote add origin git@github.com:oceans42/boluo-ui-website.git &&
+git push -f -u origin main &&
 cd -
 echo https://sparks42.top/boluo-ui-website/index.html
