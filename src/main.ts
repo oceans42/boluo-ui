e8f06041255d3f62245a3efa68fcfbb3a850e23e
sparks42 <958575100@qq.com>
2020-10-20 18:54:13 +0800

    UI框架取名

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import './index.scss'
 import { createApp } from 'vue' // 创建app实例的函数
 import App from './App.vue'
 import { router } from './router'
-import './lib/svg.js'
 import 'github-markdown-css'
 import Markdown from './components/Markdown.vue';
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,8 +3,8 @@
     <div class="topnavAndBanner">
       <Topnav />
       <div class="banner">
-        <h1>轱辘UI</h1>
-        <h2>一个厉害的 UI 框架</h2>
+        <h1>菠萝UI</h1>
+        <h2>一个平平无奇的 UI 框架</h2>
         <p class="actions">
           <a href="https://github.com">GitHub</a>
           <router-link to="/doc">开始</router-link>
