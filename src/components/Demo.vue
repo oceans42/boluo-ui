27111b7087ca5e4a91798e13a532d41bcce671cc
sparks42 <958575100@qq.com>
2020-10-20 11:27:22 +0800

    优化 Demo

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,34 +1,52 @@
 <template>
-<div class="demo">
-  <h2>{{component.__sourceCodeTitle}}</h2>
-  <div class="demo-component">
-    <component :is="component" />
-  </div>
-  <div class="demo-actions">
-    <Button>查看代码</Button>
-  </div>
-  <div class="demo-code">
-    <pre class="language-html" v-html="Prism.highlight(component.__sourceCode, Prism.languages.html, 'html')" />
-  </div>
+  <div class="demo">
+    <h2>{{component.__sourceCodeTitle}}</h2>
+    <div class="demo-component">
+      <component :is="component" />
+    </div>
+    <div class="demo-actions">
+      <Button @click="toggleCode">查看代码</Button>
+    </div>
+    <div class="demo-code"
+         v-if="codeVisible">
+      <pre class="language-html"
+           v-html="html" />
+    </div>
 
-</div>
+  </div>
 </template>
 
 <script lang="ts">
-import Button from '../lib/Button.vue'
-import 'prismjs';
-import 'prismjs/themes/prism.css'
-const Prism = (window as any).Prism
+import Button from "../lib/Button.vue";
+import "prismjs";
+import "prismjs/themes/prism.css";
+import { computed, ref } from "vue";
+const Prism = (window as any).Prism;
 export default {
-  props: {
-    component: Object
+  components: {
+    Button,
   },
-  setup() {
+  props: {
+    component: Object,
+  },
+  setup(props) {
+    const html = computed(() => {
+      return Prism.highlight(
+        props.component.__sourceCode,
+        Prism.languages.html,
+        "html"
+      );
+    });
+    const toggleCode = () => (codeVisible.value = !codeVisible.value);
+    const codeVisible = ref(false);
     return {
-      Prism
-    }
-  }
-}
+      Prism,
+      html,
+      codeVisible,
+      toggleCode,
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -36,7 +54,7 @@ $border-color: #d9d9d9;
 .demo {
   border: 1px solid $border-color;
   margin: 16px 0 32px;
-  >h2 {
+  > h2 {
     font-size: 20px;
     padding: 8px 16px;
     border-bottom: 1px solid $border-color;
@@ -51,9 +69,9 @@ $border-color: #d9d9d9;
   &-code {
     padding: 8px 16px;
     border-top: 1px dashed $border-color;
-    >pre {
+    > pre {
       line-height: 1.1;
-      font-family: Consolas, 'Courier New', Courier, monospace;
+      font-family: Consolas, "Courier New", Courier, monospace;
       margin: 0;
     }
   }
