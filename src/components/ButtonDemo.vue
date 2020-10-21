e52715e552a5838984ffa5789be3da5ef3a98d80
sparks42 <958575100@qq.com>
2020-10-21 18:03:51 +0800

    从 index 引入组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -6,7 +6,7 @@
     <Demo :component="Button3Demo" />
     <Demo :component="Button4Demo" />
     <Demo :component="Button5Demo" />
-    <Demo :component="Button6Demo" />
+    <!-- <Demo :component="Button6Demo" /> -->
   </div>
 </template>
 
@@ -17,7 +17,7 @@ import Button2Demo from "./Button2.demo.vue";
 import Button3Demo from "./Button3.demo.vue";
 import Button4Demo from "./Button4.demo.vue";
 import Button5Demo from "./Button5.demo.vue";
-import Button6Demo from "./Button6.demo.vue";
+// import Button6Demo from "./Button6.demo.vue";
 export default {
   components: { Demo },
   setup() {
@@ -27,7 +27,7 @@ export default {
       Button3Demo,
       Button4Demo,
       Button5Demo,
-      Button6Demo
+      // Button6Demo
     };
   },
 };
--- src/markdown/get-started.md
+++ src/markdown/get-started.md
@@ -4,7 +4,7 @@
 然后在你的代码中写入下面的代码
 
 ```
-import {Button, Tabs, Switch, Dialog} from "boluo-ui"
+import {Button, Tabs, Switch, SwitchPro, Dialog} from "boluo-ui"
 ```
 
 就可以使用我提供的组件了。
