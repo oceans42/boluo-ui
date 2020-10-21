b731a5c0ec71f914784da937da4bd5d5b73e366e
sparks42 <958575100@qq.com>
2020-10-21 17:28:10 +0800

    修复 Tabs 在 Vue 3 正式版的 bug

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -40,6 +40,8 @@ export default {
         const { left: left2 } = selectedItem.value.getBoundingClientRect();
         const left = left2 - left1;
         indicator.value.style.left = left + "px";
+        }, {
+        flush: 'post'
       });
     });
     const defaults = context.slots.default();
