a3a0a285bdca186a0738db0bf6d19a640dd5eaa7
sparks42 <958575100@qq.com>
2020-10-21 20:56:39 +0800

    fix

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,5 +3,5 @@ export { default as SwitchPro } from './SwitchPro.vue';
 export { default as Button } from './Button.vue';
 export { default as Tabs } from './Tabs.vue';
 export { default as Tab } from './Tab.vue';
-// export { default as Dialog } from './Dialog.vue';
-// export { openDialog as openDialog } from './openDialog';
+export { default as Dialog } from './Dialog.vue';
+export { openDialog as openDialog } from './openDialog';
